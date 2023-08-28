a64b5b23a420f9ae938a7f858040435180e8c094
Mariya Yermakova <windostre@gmail.com>
2023-08-28 21:31:07 +0300

    first commit

---
+++ src/test/java/guru/qa/SimpleTest.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
